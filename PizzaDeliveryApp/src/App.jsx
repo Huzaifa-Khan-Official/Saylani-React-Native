5dcf4a937eacf6876b7b7280213de9bcb0f9e980
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-01 20:20:09 +0500

    working with flex

--- PizzaDeliveryApp/src/App.jsx
+++ PizzaDeliveryApp/src/App.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Home from './screens/Home'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Home />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
 
 export default App
--- PizzaDeliveryApp/src/screens/Home.jsx
+++ PizzaDeliveryApp/src/screens/Home.jsx
@@ -5,7 +5,9 @@ import LinearGradient from 'react-native-linear-gradient'
 const Home = () => {
     return (
         <View style={styles.container}>
-            <LinearGradient colors={["rgba(245, 49, 63, 1)", "rgba(255, 163, 96, 1)"]} style={styles.bannerDiv}>
+            <LinearGradient colors={["rgba(245, 49, 63, 1)", "rgba(255, 163, 96, 1)"]} style={styles.bannerDiv}
+                start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+            >
                 <Text>
                     Hi Jaykey!
                 </Text>
@@ -29,24 +31,35 @@ const styles = StyleSheet.create({
     },
     bannerDiv: {
         flex: 0.2,
+        zIndex: 1,
     },
+    // card: {
+    //     shadowColor: "#000",
+    //     shadowOpacity: 1,
+    //     shadowRadius: 1,
+    //     elevation: 15,
+    // },
     card: {
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 4,
+            height: 0,
         },
-        shadowOpacity: 0.32,
-        shadowRadius: 5.46,
-        elevation: 9,
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        borderRadius: 6,
     },
     reorderDiv: {
-        flex: 0.3,
-        backgroundColor: 'blue',
+        flex: 0.4,
+        position: 'relative',
+        top: -30,
+        marginHorizontal: 10,
+        zIndex: 2
     },
     createPizzaDiv: {
-        flex: 0.3,
-        backgroundColor: 'green',
+        flex: 0.4,
+        marginHorizontal: 10,
     },
     fontBold: {
         fontWeight: 'bold',
