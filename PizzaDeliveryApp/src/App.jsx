018f18c73cf5d084d71e64fa0b566c916c5d55ab
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-02 22:29:07 +0500

    BannerComponent Done

--- PizzaDeliveryApp/src/App.jsx
+++ PizzaDeliveryApp/src/App.jsx
@@ -1,10 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 import React from 'react'
 import Home from './screens/Home'
 
 const App = () => {
   return (
     <View style={styles.container}>
+      <StatusBar translucent backgroundColor="transparent"/>
       <Home />
     </View>
   )
--- PizzaDeliveryApp/src/screens/Home.jsx
+++ PizzaDeliveryApp/src/screens/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <LinearGradient colors={["rgba(245, 49, 63, 1)", "rgba(255, 163, 96, 1)"]} style={styles.bannerDiv}
                 start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
             >
-                <Text>
+                <Text style={styles.titleText}>
                     Hi Jaykey!
                 </Text>
                 <Text style={styles.bannerTextDiv}>
@@ -29,33 +29,36 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    titleText: {
+        fontSize: 28,
+        color: "white",
+        fontWeight: "bold",
+    },
     bannerDiv: {
         flex: 0.2,
         zIndex: 1,
+        paddingHorizontal: 20,
+        gap: 8,
+        paddingTop: 24,
     },
-    // card: {
-    //     shadowColor: "#000",
-    //     shadowOpacity: 1,
-    //     shadowRadius: 1,
-    //     elevation: 15,
-    // },
     card: {
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 0,
+            height: 5,
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
-        elevation: 5,
-        borderRadius: 6,
+        elevation: 20,
+        borderRadius: 20,
+        backgroundColor: "white"
     },
     reorderDiv: {
         flex: 0.4,
         position: 'relative',
         top: -30,
         marginHorizontal: 10,
-        zIndex: 2
+        zIndex: 2,
     },
     createPizzaDiv: {
         flex: 0.4,
@@ -65,9 +68,12 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     bannerTextDiv: {
-        borderRightWidth: 2,
+        borderLeftWidth: 2,
+        paddingHorizontal: 20,
+        fontSize: 22,
         borderColor: "white",
         color: "white",
+        marginBottom: 10,
     }
 })
 
